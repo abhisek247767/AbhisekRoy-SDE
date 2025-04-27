7c077b5fbbe04679b0b053630084583b522a5406
AbhisekRoy-1 <>
2025-04-27 23:18:36 +0530

    update structure for testimonials and Tic Tac Toe section

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 /*==================== SHOW MENU ====================*/
 const navMenu = document.getElementById("nav-menu");
-navToggle = document.getElementById("nav-toggle");
-navClose = document.getElementById("nav-close");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
 if (navToggle) {
   navToggle.addEventListener("click", () => {
@@ -18,22 +18,22 @@ if (navClose) {
 const navLink = document.querySelectorAll(".nav_link");
 
 function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
+  // No need to redeclare navMenu here, just use the globally declared one
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== SKILLS ====================*/
 const skillsContent = document.getElementsByClassName("skills_content");
-skillsHeader = document.querySelectorAll(".skills_header");
+const skillsHeader = document.querySelectorAll(".skills_header");
 
 function toggleSkills() {
   let itemClass = this.parentNode.className;
 
-  for (i = 0; i < skillsContent.length; i++) {
+  for (let i = 0; i < skillsContent.length; i++) {
     skillsContent[i].className = "skills_content skills_close";
   }
-  if (itemClass == "skills_content skills_close") {
+  if (itemClass === "skills_content skills_close") {
     this.parentNode.className = "skills_content skills_open";
   }
 }
@@ -77,7 +77,7 @@ function scrollActive() {
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+    const sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
